e553d2c3b559c0d08717bef4ccb4b68d13cfe1a3
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-18 14:29:36 +0530

    new: page navigation to manage menu links

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,9 @@
 # version: 0.0.1
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
 # url: https://github.com/vinkas0/discourse-navigation
+
+add_admin_route 'navigation.title', 'navigation'
+
+Discourse::Application.routes.append do
+  get '/admin/plugins/navigation' => 'admin/plugins#index', constraints: StaffConstraint.new
+end
