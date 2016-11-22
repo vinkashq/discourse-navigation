9523e6c135066fd2d4bf9e309d3ba119160ad692
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-23 00:58:25 +0530

    version bump to 0.0.2

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: navigation
 # about: Plugin to add a custom nav menu links
-# version: 0.0.1
+# version: 0.0.2
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
 # url: https://github.com/vinkas0/discourse-navigation
 
